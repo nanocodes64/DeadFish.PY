11a3165fe163c15d7b1d90702af588a966e1ec5c
nanocodes64 <nanocodesyt@gmail.com>
2023-01-05 12:57:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Deadfish interpreter in Python
+Hi, I just made this for fun. It's in pure python and there is no dependencies. The code may be a bit wonky but it works.
---
+++ deadfish.py
@@ -0,0 +1,49 @@
+class Program:
+    #instructions
+    I_INC = 'i'
+    I_DEC = 'd'
+    I_SQR = 's'
+    I_OUT = 'o'
+
+    def __init__(self, data):
+        self.accumulator = 0
+        self.data = data
+    def CheckOverflow(self):
+        if self.accumulator > 255 or self.accumulator < 0:
+            self.accumulator = 0
+    def Run(self): # the actual interpreter
+        for i in self.data:
+            curr_instruction = i.lower()
+            if curr_instruction == Program.I_INC:
+                self.Increment()
+            elif curr_instruction == Program.I_DEC:
+                self.Decrement()
+            elif curr_instruction == Program.I_SQR:
+                self.Square()
+            elif curr_instruction == Program.I_OUT:
+                self.Output()
+            self.CheckOverflow()
+    def Increment(self):
+        self.accumulator += 1
+
+    def Decrement(self):
+        self.accumulator -= 1
+
+    def Square(self):
+        self.accumulator **= 2
+
+    def Output(self):
+        print(self.accumulator)
+if __name__ == '__main__':
+    prg = input("deadfish1.0\nWhich file to load? X for manual input>")
+    if prg != "X":
+        try:
+            with open(prg, 'r') as f:
+                prg = f.read()
+        except:
+            print("Failed to load! Entering manual input mode.")
+            prg = input("Enter deadfish program:")
+    else:
+        prg = input("Enter deadfish program:")
+    x = Program(prg)
+    x.Run()
